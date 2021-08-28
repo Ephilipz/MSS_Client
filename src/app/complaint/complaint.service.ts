b6d7e4f51a518f10a30cd61a56b2093d7026814c
Huy Tran <htt5059@psu.edu>
2021-08-27 21:26:52 -0400

    add enum complaint status, set up complaint routes

--- src/app/complaint/complaint.service.ts
+++ src/app/complaint/complaint.service.ts
@@ -1,9 +1,12 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ComplaintService {
-
-  constructor() { }
+  private readonly baseUrl = environment.baseUrl + "Complaint";
+  
+  constructor(private http: HttpClient) { }
 }
---
+++ src/app/complaint/enums/complaintStatus.enums.ts
@@ -0,0 +1,3 @@
+export enum ComplaintStatus {
+    Open, Pending, Closed
+}
